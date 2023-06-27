5ea47a4380c1da6d26c34da082c3adaf328e1f5b
chenyungui <286224043@qq.com>
2023-06-27 14:04:39 +0800

    unspecified

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     	)
     ],
     dependencies: [
-        .package(url: "git@github.com:chenyunguiMilook/CommonKit.git", from: "0.1.96"),
+        .package(url: "git@github.com:chenyunguiMilook/CommonKit.git", from: "0.1.97"),
     ],
     targets: [
     	.target(

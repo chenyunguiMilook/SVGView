426b21ce0b5609d1d1529ecc17fd5d0842daba6d
chenyungui <286224043@qq.com>
2023-06-29 11:53:10 +0800

    prepare for create symbol path

---
+++ SVGViewTests/SymbolTest.swift
@@ -0,0 +1,47 @@
+//
+//  SymbolTest.swift
+//  
+//
+//  Created by chenyungui on 2023/6/29.
+//
+
+import XCTest
+
+final class SymbolTest: XCTestCase {
+
+    let path = "/Users/chenyungui/Desktop/circle.svg"
+    
+    let regularLeft: Double = 1391.32
+    let regularRight: Double = 1508.37
+    
+    let smallTop: Double = 625.54
+    let smallBottom: Double = 696
+    
+    let mediumTop: Double = 1055.54
+    let mediumBottom: Double = 1126
+    
+    let largeTop: Double = 1485.54
+    let largeBottom: Double = 1556
+    
+    let ultralightCenterX: Double = 559.71127
+    let thinCenterX: Double = 856.4225700000001
+    let lightCenterX: Double = 1153.13052
+    let regularCenterX: Double = 1449.84467
+    let mediumCenterX: Double = 1746.5560699999999
+    let semiboldCenterX: Double = 2043.26982
+    let boldCenterX: Double = 2339.97637
+    let heavyCenterX: Double = 2636.69307
+    let blackenterX: Double = 2933.39812
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+}

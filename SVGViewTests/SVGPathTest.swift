18ea9ff14b7fe939b49f6fd9a544dec939de5359
chenyunguiMilook <linxitj2003@icloud.com>
2024-09-06 15:55:52 +0800

    fix error

--- SVGViewTests/SVGPathTest.swift
+++ SVGViewTests/SVGPathTest.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import XCTest
-import CommonKit
+import CoreKit
 
 @testable import SVGView
 
@@ -15,7 +15,7 @@ class SVGPathTests: XCTestCase {
     
     let svgString = "M45.3125-73.4375C43.3105-73.4375 41.6992-75.0977 41.6992-77.0508C41.6992-79.0039 43.3105-80.6152 45.3125-80.6152C47.3145-80.6152 48.9258-79.0039 48.9258-77.0508C48.9258-75.0977 47.3145-73.4375 45.3125-73.4375Z"
     
-    func testSVGToBezierPath() {
+    @MainActor func testSVGToBezierPath() {
         let reader = PathReader(input: svgString)
         let segments = reader.read()
         let path = segments.toBezierPath()

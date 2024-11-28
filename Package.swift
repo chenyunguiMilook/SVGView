3cf2b299022cb39a85f87fc56501dffa95b89c42
chenyunguiMilook <linxitj2003@icloud.com>
2024-11-28 21:20:32 +0800

    unspecified

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     	)
     ],
     dependencies: [
-        .package(url: "git@github.com:chenyunguiMilook/CoreKit.git", from: "1.0.8"),
+        .package(url: "git@github.com:chenyunguiMilook/CoreKit.git", from: "1.0.9"),
     ],
     targets: [
     	.target(

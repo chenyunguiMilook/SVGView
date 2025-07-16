c8acc21ecfab5c558a3ab277e0ff0bb7664209ac
chenyunguiMilook <linxitj2003@icloud.com>
2025-07-16 10:27:27 +0800

    unspecified

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "git@github.com:chenyunguiMilook/PrimeKit.git", from: "0.0.13"),
-        .package(url: "git@github.com:chenyunguiMilook/RenderKit.git", from: "0.0.11"),
+        .package(url: "git@github.com:chenyunguiMilook/RenderKit.git", from: "0.0.12"),
     ],
     targets: [
     	.target(
